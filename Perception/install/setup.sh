1bb49f0df09833b3c67e8ea82137e7c8bbcb5fec
anton <anton.n.cecire@student.uts.edu.au>
2025-04-09 14:52:14 +1000

    perception running

--- Perception/install/setup.sh
+++ Perception/install/setup.sh
@@ -35,7 +35,7 @@ COLCON_CURRENT_PREFIX="/home/anton/ros2_ws/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/anton/git/LoversOfMen/install"
+COLCON_CURRENT_PREFIX="/home/anton/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
 
