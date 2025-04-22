6dd6b024690c35a49a65ec4aff37b8c852a7eb98
anton <anton.n.cecire@student.uts.edu.au>
2025-04-22 14:06:02 +1000

    more buttons

--- ur3-realsense-gui/src/gui/main_window.py
+++ ur3-realsense-gui/src/gui/main_window.py
@@ -6,7 +6,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("UR3 and RealSense Control")
-        self.setGeometry(100, 100, 300, 250)  # Adjusted height to fit the new button
+        self.setGeometry(100, 100, 300, 300)  # Adjusted height to fit the new buttons
 
         layout = QVBoxLayout()
 
@@ -19,18 +19,28 @@ class MainWindow(QMainWindow):
         self.start_realsense_button = QPushButton("Start RealSense Camera")
         self.start_realsense_button.clicked.connect(self.start_realsense)
 
-        self.stop_realsense_button = QPushButton("Stop RealSense Camera")
-        self.stop_realsense_button.clicked.connect(self.stop_realsense)
+        # Changed "Stop RealSense Camera" to "Capture Image"
+        self.capture_image_button = QPushButton("Capture Image")
+        self.capture_image_button.clicked.connect(self.capture_image)
+
+        # Adding the gripper buttons
+        self.open_gripper_button = QPushButton("Open Gripper")
+        self.open_gripper_button.clicked.connect(self.open_gripper)
+
+        self.close_gripper_button = QPushButton("Close Gripper")
+        self.close_gripper_button.clicked.connect(self.close_gripper)
 
         # Adding the E-Stop button
         self.estop_button = QPushButton("E-Stop")
         self.estop_button.setStyleSheet("background-color: red; color: white; font-weight: bold;")
         self.estop_button.clicked.connect(self.estop)
 
+        layout.addWidget(self.start_realsense_button)
+        layout.addWidget(self.capture_image_button)  # Add the Capture Image button
         layout.addWidget(self.start_ur3_button)
         layout.addWidget(self.stop_ur3_button)
-        layout.addWidget(self.start_realsense_button)
-        layout.addWidget(self.stop_realsense_button)
+        layout.addWidget(self.open_gripper_button)  # Add the Open Gripper button
+        layout.addWidget(self.close_gripper_button)  # Add the Close Gripper button
         layout.addWidget(self.estop_button)  # Add the E-Stop button to the layout
 
         container = QWidget()
@@ -46,13 +56,19 @@ class MainWindow(QMainWindow):
     def start_realsense(self):
         print("Starting RealSense Camera...")
 
-    def stop_realsense(self):
-        print("Stopping RealSense Camera...")
+    def capture_image(self):
+        print("Capturing Image...")
 
     def estop(self):
         print("Emergency Stop Activated!")
         self.stop_ur3()
         self.stop_realsense()
+
+    def close_gripper(self):
+        print("Closing Gripper...")
+
+    def open_gripper(self):
+        print("Opening Gripper...")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
