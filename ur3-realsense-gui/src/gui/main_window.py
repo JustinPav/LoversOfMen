55f5bf906a600ad5270b1c763d4f99b8a95bddfb
anton <anton.n.cecire@student.uts.edu.au>
2025-04-22 13:59:47 +1000

    moved buttons fr this time

--- ur3-realsense-gui/src/gui/main_window.py
+++ ur3-realsense-gui/src/gui/main_window.py
@@ -38,9 +38,10 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.stop_ur3_button)
         layout.addWidget(self.start_realsense_button)
         layout.addWidget(self.stop_realsense_button)
-        layout.addWidget(self.estop_button)  # Add the E-Stop button to the layout
-        layout.addWidget(self.close_gripper_button)  # Add the Close Gripper button
         layout.addWidget(self.open_gripper_button)  # Add the Open Gripper button
+        layout.addWidget(self.close_gripper_button)  # Add the Close Gripper button
+        layout.addWidget(self.estop_button)  # Add the E-Stop button to the layout
+
 
         container = QWidget()
         container.setLayout(layout)
