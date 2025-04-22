6a5dab3da6608bc905021a7fcb324bbdb0486390
anton <anton.n.cecire@student.uts.edu.au>
2025-04-22 13:55:46 +1000

    added more buttons

--- ur3-realsense-gui/src/gui/main_window.py
+++ ur3-realsense-gui/src/gui/main_window.py
@@ -6,7 +6,7 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("UR3 and RealSense Control")
-        self.setGeometry(100, 100, 300, 200)
+        self.setGeometry(100, 100, 300, 300)  # Adjusted height to fit the new buttons
 
         layout = QVBoxLayout()
 
@@ -22,10 +22,25 @@ class MainWindow(QMainWindow):
         self.stop_realsense_button = QPushButton("Stop RealSense Camera")
         self.stop_realsense_button.clicked.connect(self.stop_realsense)
 
+        # Adding the E-Stop button
+        self.estop_button = QPushButton("E-Stop")
+        self.estop_button.setStyleSheet("background-color: red; color: white; font-weight: bold;")
+        self.estop_button.clicked.connect(self.estop)
+
+        # Adding the gripper buttons
+        self.close_gripper_button = QPushButton("Close Gripper")
+        self.close_gripper_button.clicked.connect(self.close_gripper)
+
+        self.open_gripper_button = QPushButton("Open Gripper")
+        self.open_gripper_button.clicked.connect(self.open_gripper)
+
         layout.addWidget(self.start_ur3_button)
         layout.addWidget(self.stop_ur3_button)
         layout.addWidget(self.start_realsense_button)
         layout.addWidget(self.stop_realsense_button)
+        layout.addWidget(self.estop_button)  # Add the E-Stop button to the layout
+        layout.addWidget(self.close_gripper_button)  # Add the Close Gripper button
+        layout.addWidget(self.open_gripper_button)  # Add the Open Gripper button
 
         container = QWidget()
         container.setLayout(layout)
@@ -42,6 +57,17 @@ class MainWindow(QMainWindow):
 
     def stop_realsense(self):
         print("Stopping RealSense Camera...")
+
+    def estop(self):
+        print("Emergency Stop Activated!")
+        self.stop_ur3()
+        self.stop_realsense()
+
+    def close_gripper(self):
+        print("Closing Gripper...")
+
+    def open_gripper(self):
+        print("Opening Gripper...")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
